473dfda427f52642c6467885333ebe201f0a08cc
tatsuma-sato <t.tsuma.s@gmail.com>
2021-10-06 00:05:01 -0700

    created shopping cart

--- js/main.js
+++ js/main.js
@@ -1,86 +1,163 @@
 let cartItems = new Array();
 
-
-$(document).ready(function() {
-    $('.nice-textbox').blur(function() {
-            if ($(this).val().length === 0) {
-                $('.nice-label').removeClass("focus");
-            } else { returns; }
-        })
-        .focus(function() {
-            $('.nice-textLabel').addClass("focus")
-        });
-    $('.nice-emailbox').blur(function() {
-            if ($(this).val().length === 0) {
-                $('.nice-label').removeClass("focus");
-            } else { returns; }
-        })
-        .focus(function() {
-            $('.nice-emailLabel').addClass("focus")
-        });
-    $('.nice-textareabox').blur(function() {
-            if ($(this).val().length === 0) {
-                $('.nice-label').removeClass("focus");
-            } else { returns; }
-        })
-        .focus(function() {
-            $('.nice-textareaLabel').addClass("focus")
-        });
-
-    $('.purchase-button').on('click', function() {
-        let priceText = $(this).parent().parent().find('.price').text()
-        let price =priceText.replace("Price: $", "");
-        let color = $(this).parent().parent().find('[name=color] option:selected').val()
-        let img = $(this).parent().parent().find('.car-img').attr('src')
-        cartItems.push({'price': price, 'color': color, 'img': img})
-    });
-
-    const carA = {
-        price: 300,
-        color: 'black',
-    }
-    const carB = {
-        price: 500,
-        color: 'red',
-    }
-    const carC = {
-        price: 600,
-        color: 'white',
-    }
-
-
-    cartItems.push(carA,carB,carC);
-    console.log(cartItems);
-
-    // cartItems.forEach(function(){
-    //     console.log(price);
-    // })
-
-    $(cartItems).each(function(){
-        const productPrice = this.price;
-        const productColor = this.color;
-        console.log(productPrice);
-        console.log(productColor);
-
-        
+$(document).ready(function () {
+  $(".nice-textbox")
+    .blur(function () {
+      if ($(this).val().length === 0) {
+        $(".nice-label").removeClass("focus");
+      } else {
+        returns;
+      }
     })
-
-    const subTotal = Number($('#cart-subtotal').text());
-    const tax = Number($('#cart-tax').text());
-    const shipping = Number($('#cart-shipping').text());
-    const grandTotal = Number($('#cart-total').text());
-    // console.log(subTotal);
-    // console.log(tax);
-    // console.log(shipping);
-    // console.log(grandTotal);
-});
-$(window).scroll(function() {
-    $('.fadein').each(function() {
-        var elemPos = $(this).offset().top;
-        var scroll = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        if (scroll > elemPos - windowHeight + 200) {
-            $(this).addClass('scrollin');
-        }
+    .focus(function () {
+      $(".nice-textLabel").addClass("focus");
     });
+  $(".nice-emailbox")
+    .blur(function () {
+      if ($(this).val().length === 0) {
+        $(".nice-label").removeClass("focus");
+      } else {
+        returns;
+      }
+    })
+    .focus(function () {
+      $(".nice-emailLabel").addClass("focus");
+    });
+  $(".nice-textareabox")
+    .blur(function () {
+      if ($(this).val().length === 0) {
+        $(".nice-label").removeClass("focus");
+      } else {
+        returns;
+      }
+    })
+    .focus(function () {
+      $(".nice-textareaLabel").addClass("focus");
+    });
+
+  $(".purchase-button").on("click", function () {
+    let priceText = $(this).parent().parent().find(".price").text();
+    let price = priceText.replace("Price: $", "");
+    let color = $(this)
+      .parent()
+      .parent()
+      .find("[name=color] option:selected")
+      .val();
+    let img = $(this).parent().parent().find(".car-img").attr("src");
+    cartItems.push({ price: price, color: color, img: img });
+  });
+
+  // Shopping Cart ( Tatsuma )
+
+  const cartButton = document.querySelector("#cart");
+  const shoppingCart = document.querySelector(".shopping-cart");
+  const arrow = document.querySelector(".arrow");
+  cartButton.addEventListener("click", function () {
+    shoppingCart.classList.toggle("hidden");
+    cartButton.classList.toggle("opened");
+    arrow.classList.toggle("arrow-up");
+  });
+
+  const productList = document.querySelector("#product-list");
+
+  // set rate
+  const TAXRATE = 0.1;
+  const SHIPPINGRATE = 15.0;
+
+  // calculating cart
+
+  const calculateCart = function () {
+    let subTotal = 0;
+
+    $(".product").each(function () {
+      const removeComma = Number($(this.children[4]).text().replace(",", ""));
+      // console.log(Number($(this.children[4]).text()));
+      subTotal += removeComma;
+      console.log(subTotal);
+    });
+
+    let tax = subTotal * TAXRATE;
+    let shipping = subTotal > 0 ? SHIPPINGRATE : 0;
+    let grandTotal = subTotal + tax + shipping;
+
+    $("#cart-subtotal").html(
+      subTotal.toLocaleString(undefined, { minimumFractionDigits: 2 })
+    );
+    $("#cart-tax").html(
+      tax.toLocaleString(undefined, { minimumFractionDigits: 2 })
+    );
+    $("#cart-shipping").html(
+      shipping.toLocaleString(undefined, { minimumFractionDigits: 2 })
+    );
+    $("#cart-total").html(
+      grandTotal.toLocaleString(undefined, { minimumFractionDigits: 2 })
+    );
+  };
+
+  // assign action
+
+  $(".purchase-button").on("click", function () {
+    alert("added to cart!");
+
+    $(cartItems).each(function () {
+      let productPrice = this.price;
+      productPrice = Number(productPrice).toLocaleString();
+
+      let productColor = this.color;
+
+      let productImg = this.img;
+
+      const newProduct = document.createElement("div");
+      newProduct.setAttribute("class", "product");
+      newProduct.setAttribute("id", "product");
+
+      newProduct.innerHTML = `<div class="product-image" id="product-image"><img src="${productImg}" alt=""></div>    <div class="product-details" id="product-details"><div class="product-title">Car Name</div><p class="product-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem perferendis aperiam facilis illum illo! Eveniet!</p></div><div class="product-color" id="product-color"><select name="color" id="color"><option value="black">Black</option><option value="red">Red</option><option value="Blue">Blue</option></select></div><div class="product-removal" id="product-removal"><button class="remove-product btn">Remove</button></div><div class="product-line-price" id="product-line-price">${productPrice}</div>`;
+
+      const newProductColor = newProduct
+        .getElementsByTagName("select")
+        .item(0).childNodes;
+
+      if (productColor === "Black") {
+        newProductColor[0].setAttribute("selected", true);
+      } else if (productColor === "Red") {
+        newProductColor[1].setAttribute("selected", true);
+      } else if (productColor === "Blue") {
+        newProductColor[2].setAttribute("selected", true);
+      }
+
+      productList.appendChild(newProduct);
+
+      cartItems.pop();
+    });
+  });
+
+  $("#cart").on("click", function () {
+    $(".product-removal button").click(function () {
+      removeItem(this);
+    });
+
+    calculateCart();
+
+    cartItems = [];
+  });
+
+  const removeItem = function (removeButton) {
+    console.log("clicked");
+    let prdocutRow = $(removeButton).parent().parent();
+    console.log(prdocutRow);
+    prdocutRow.remove();
+    calculateCart();
+  };
+
+  // End Shopping Cart
+});
+$(window).scroll(function () {
+  $(".fadein").each(function () {
+    var elemPos = $(this).offset().top;
+    var scroll = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    if (scroll > elemPos - windowHeight + 200) {
+      $(this).addClass("scrollin");
+    }
+  });
 });
